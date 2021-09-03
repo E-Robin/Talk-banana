7a5ab9830c6993fbca1c376386a7022af26b1620
E-Robin <grobin578@gmail.com>
2021-09-03 16:05:08 +0530

    js updated

--- talk.js
+++ talk.js
@@ -1,6 +1,23 @@
 var btnTranslate=document.querySelector("#btn-transfer");
+var txtInput=document.querySelector("#txt-input");
+var divOutput=document.querySelector("#output");
 
-btnTranslate.addEventListener("click",function clickEventHandler ()
-{
-    console.log("clicked");
-})
+// divOutput.innerText="Robin singh Gusain"
+document.getElementById("output").innerText="robin singh is great "
+
+
+
+
+
+btnTranslate.addEventListener("click",clickHandler);
+function clickHandler(){
+    console.log("clickedd!!!");
+    console.log("input:", txtInput.value);
+    divOutput.innerText="hey robin :  " + txtInput.value;
+}
+
+// btnTranslate.addEventListener("click",function clickEventHandler ()
+// {
+//     console.log("clicked");
+// })
+
